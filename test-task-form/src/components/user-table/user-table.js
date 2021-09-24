b331f6d9bf0cd801b18cbe8ada1f0e4f3424ac00
Karina Zhuro <kzhuro36@gmail.com>
2021-09-24 19:24:54 +0300

    feat: add icon sort

--- test-task-form/src/components/user-table/user-table.js
+++ test-task-form/src/components/user-table/user-table.js
@@ -3,6 +3,8 @@ import React from 'react';
 import "./user-table.css";
 
 const UserTable = ({people, onClickRow}) => {
+	const imgSort = <span className="material-icons arrow-drop-down">arrow_drop_down</span>;
+
 	const renderPeople = () => {
 		return people.map(person => {
 			const {id, firstName, lastName, email, phone, adress: {state}} = person;
@@ -24,12 +26,12 @@ const UserTable = ({people, onClickRow}) => {
 		<table>
 			<thead>
 			<tr>
-				<th>id</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th>Email</th>
-				<th>Phone</th>
-				<th>State</th>
+				<th>id {imgSort}</th>
+				<th>First Name {imgSort}</th>
+				<th>Last Name {imgSort}</th>
+				<th>Email {imgSort}</th>
+				<th>Phone {imgSort}</th>
+				<th>State {imgSort}</th>
 			</tr>
 			</thead>
 			<tbody>
